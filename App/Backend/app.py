9f539864828624dd5a59a42a535e1eb2c750ed9e
Dibyajyoti Mohanty <dibyamohanty069@gmail.com>
2024-05-29 10:26:30 +0530

    keys

--- App/Backend/app.py
+++ App/Backend/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, jsonify, send_file
 from flask_cors import CORS
 import requests
 import os
-import openai
+from openai import OpenAI
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -10,7 +10,12 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+#OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+@app.route('/')  # Add a route for the root path
+def index():
+    return 'Welcome to my Flask App!'
 
 @app.route('/generate-image', methods=['POST'])
 def generate_image():
@@ -20,15 +25,16 @@ def generate_image():
     if not prompt:
         return jsonify({'error': 'Prompt is required'}), 400
 
-    response = openai.Image.create(
+    response = client.images.generate(
         model="dall-e-3",
         prompt=prompt,
         size="1024x1024",
+        quality="standard",
         n=1
     )
 
-    image_url = response['data'][0]['url']
+    image_url = response.data[0].url
     return jsonify({'image_url': image_url})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
